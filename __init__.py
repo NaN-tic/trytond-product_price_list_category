3f90e8b6bcc7678ea668dc3caa8ad6884531cbaf
resteve <resteve@zikzakmedia.com>
2012-09-25 13:52:27 +0200

    Initial release

---
+++ __init__.py
@@ -0,0 +1,5 @@
+#This file is part product_price_list_category module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains 
+#the full copyright notices and license terms.
+
+from .price_list import *
---
+++ doc/index.rst
@@ -0,0 +1,4 @@
+Product Price List Category Module
+##################################
+
+The product_price_list_category module add in price list a new condition line: category.
