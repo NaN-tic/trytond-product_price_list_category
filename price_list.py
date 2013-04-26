50fbbdf34fcdf6fbc21497d03e891c1bfafca4b3
resteve <resteve@zikzakmedia.com>
2013-04-26 14:12:50 +0200

    pyflakes fixed

--- price_list.py
+++ price_list.py
@@ -1,9 +1,7 @@
 #This file is part product_price_list_category module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
-
-from trytond.model import ModelView, ModelSQL, fields
-from trytond.tools import safe_eval, datetime_strftime
+from trytond.model import fields
 from trytond.transaction import Transaction
 from trytond.pool import Pool, PoolMeta
 
