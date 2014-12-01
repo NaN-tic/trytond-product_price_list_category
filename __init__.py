316e9b870c58d26f69cc23bc1148e9d75c7ed448
jmartin <jmartin@zikzakmedia.com>
2014-12-01 12:40:36 +0100

    pep8

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-#This file is part product_price_list_category module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part of product_price_list_category module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from trytond.pool import Pool
 from .price_list import *
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
-#This file is part product_price_list_category module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part of product_price_list_category module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from .test_product_price_list_category import suite
 
